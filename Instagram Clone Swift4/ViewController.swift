c49592eb4a4883241ef595a3930361d5ca10db60
Chaman Gurjar <chamangujjar162@gmail.com>
2019-01-06 16:01:00 +0530

    Initial Commit

---
+++ Instagram Clone Swift4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram Clone Swift4
+//
+//  Created by Chaman Gurjar on 06/01/19.
+//  Copyright © 2019 Chaman Gurjar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
