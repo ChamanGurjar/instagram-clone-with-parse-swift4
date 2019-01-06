3046be52c6f0efc851449af92c47608c000ae1bb
Chaman Gurjar <chamangujjar162@gmail.com>
2019-01-06 17:05:46 +0530

    Parse configured and dummy object saved to parse server

--- Instagram Clone Swift4/ViewController.swift
+++ Instagram Clone Swift4/ViewController.swift
@@ -7,14 +7,30 @@
 //
 
 import UIKit
+import Parse
 
+//Parse server is hosted on -> https://back4app.com
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        saveSampleObjectToParseServer()
     }
-
-
+    
+    private func saveSampleObjectToParseServer() {
+        let gameScore = PFObject(className:"GameScore")
+        gameScore["score"] = 1337
+        gameScore["playerName"] = "Sean Plott"
+        gameScore["cheatMode"] = false
+        gameScore.saveInBackground {
+            (success: Bool, error: Error?) in
+            if (success) {
+                print("The object has been saved.")
+            } else {
+                print(error)
+            }
+        }
+    }
+    
 }
 
