2fcf012574c7d2b4803173742d4b7ddc3b653b6a
Chaman Gurjar <chamangujjar162@gmail.com>
2019-01-09 23:04:35 +0530

    Image uploaded to parse server with comment.

--- Instagram Clone Swift4/PostViewController.swift
+++ Instagram Clone Swift4/PostViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 class PostViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
@@ -38,7 +39,35 @@ class PostViewController: UIViewController, UINavigationControllerDelegate, UIIm
     }
     
     @IBAction func postImage(_ sender: UIButton) {
-        
+        if let image = postImageView.image {
+            let post = PFObject(className: "Post")
+            post["userId"] = PFUser.current()?.objectId
+            post["comment"] = commentTF.text!
+            
+            if let imageData = image.jpegData(compressionQuality: 1) {
+                let imageFile = PFFile(name: "image.png", data: imageData)
+                post["imageFile"] = imageFile
+                
+                post.saveInBackground { (success, err) in
+                    if success {
+                        self.showAlert(title: "Hurrah, Success", message: "Your image has been posted successfully")
+                        self.commentTF.text = ""
+                        self.postImageView.image = nil
+                    } else {
+                        self.showAlert(title: "Uhhh, Error", message: "Sorry image couldn't be posted. Please try again.")
+                    }
+                }
+            }
+        }
     }
     
+    
+    private func showAlert(title: String,  message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action) in
+            self.dismiss(animated: true, completion: nil)
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
 }
