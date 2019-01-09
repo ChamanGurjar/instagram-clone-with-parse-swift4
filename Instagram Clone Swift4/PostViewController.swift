cb6477bab74021d4934c04d7454b0813a919f503
Chaman Gurjar <chamangujjar162@gmail.com>
2019-01-09 22:40:42 +0530

    Image choosen from library and selected image shown on screen

--- Instagram Clone Swift4/PostViewController.swift
+++ Instagram Clone Swift4/PostViewController.swift
@@ -8,21 +8,33 @@
 
 import UIKit
 
-class PostViewController: UIViewController {
-
+class PostViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
     @IBOutlet private weak var postImageView: UIImageView!
     @IBOutlet weak var commentTF: UITextField!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
     
     @IBAction func chooseImage(_ sender: UIButton) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage {
+            postImageView.image = image
+        }
         
+        self.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func postImage(_ sender: UIButton) {
