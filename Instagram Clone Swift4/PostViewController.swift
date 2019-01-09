d1e4164d09419df987c5f13bb0a2d5b9174f035c
Chaman Gurjar <chamangujjar162@gmail.com>
2019-01-09 22:25:25 +0530

    Post Image UI Added. PostViewController class Added. IBOutlets And Actions Created.

---
+++ Instagram Clone Swift4/PostViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PostViewController.swift
+//  Instagram Clone Swift4
+//
+//  Created by Chaman Gurjar on 09/01/19.
+//  Copyright © 2019 Chaman Gurjar. All rights reserved.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    @IBOutlet private weak var postImageView: UIImageView!
+    @IBOutlet weak var commentTF: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func chooseImage(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func postImage(_ sender: UIButton) {
+        
+    }
+    
+}
